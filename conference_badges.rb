c76628353501cd3394c630347be04ec944f77a41
James Dodds <dodds1990@gmail.com>
2020-03-05 21:36:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  badges =[]
+  array.each do |element|
+    badges << badge_maker(element)
+  end
+  badges
+end
